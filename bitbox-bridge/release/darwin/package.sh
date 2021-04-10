28550b9469319d5ccd75151e530569f7c0c35f52
Niklas Dusenlund <niklas@dusenlund.se>
2021-04-10 23:05:55 +0200

    wip

--- bitbox-bridge/release/darwin/package.sh
+++ bitbox-bridge/release/darwin/package.sh
@@ -10,6 +10,9 @@ VERSION=$(toml-echo bitbox-bridge/Cargo.toml package.version)
 
 (
 	cd bitbox-bridge/release/darwin
+	codesign -s "${IDENTITY}" tmp/opt/shiftcrypto/bitbox-bridge/bin/bitbox-bridge
 	pkgbuild --root tmp --scripts scripts --identifier ch.shiftcrypto.bitboxbridge --version "${VERSION}" --ownership recommended bridge.pkg
+	codesign -s "${IDENTITY}" bridge.pkg
 	productbuild --distribution distribution.xml --resources resources --package-path . --version "${VERSION}" "${NAME}-${VERSION}-macOS-installer.pkg"
+	codesign -s "${IDENTITY}" "${NAME}-${VERSION}-macOS-installer.pkg"
 )
